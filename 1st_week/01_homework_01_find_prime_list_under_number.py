ad0c661de002f3961615e57d3cae02452fdc7b87
tkdlek0501 <tkdlek0501@naver.com>
2025-04-07 19:39:53 +0900

    find alphabet, number

--- 1st_week/01_homework_01_find_prime_list_under_number.py
+++ 1st_week/01_homework_01_find_prime_list_under_number.py
@@ -5,15 +5,64 @@
 # [2, 3, 5, 7, 11, 13, 17, 19]
 
 # 아이디어
-# 1. 정수를 입력 받고 그 이하의 값 중 소수면 array 에 넣어둬야 한다
-# 2. 소수인지를 검사하려면? 자신보다 작은 두 개의 자연수를 곱하여 만들 수 없어야 하고 1보다 커야 한다
 
 input = 20
 
+# 1. 소수는 자기 자신과 1 외에는 아무 것도 나눌 수 없다.
+# def find_prime_list_under_number(number):
+#     prime_list = []
+#
+#     # 2~number 까지 찾아서 이것들이 소수이면 prime_list 넣는다.
+#     for n in range(2, number + 1): # 2 ~ n 까지의 숫자들이 n에 들어가는 것을 반복한다
+#         # 이것들이 소수인가?
+#         print(prime_list)
+#         for i in range(2, n): # 2부터 n-1 까지 i 에 들어가는 것을 반복 (1과 자기 자신 제외)
+#             if n % i == 0: # 나누어 떨어지면 소수가 아니다
+#                 break
+#         else:
+#             prime_list.append(n)
+#     return prime_list
+# 위 방식은 효율적이지 않다.
+# n이 19라고 가정한다면 i는 2 ~ 18 이 되는데
+# 2와 3으로 나누어 떨어지지 않으면 4, 6 등은 당연히 안 나누어 떨어질텐데 for문을 돌아야 한다
+# 즉 소수로 나누어 떨어지는 지만 확인하면 된다
+
+# def find_prime_list_under_number(number):
+#     prime_list = []
+#
+#     # 2~number 까지 찾아서 이것들이 소수이면 prime_list 넣는다.
+#     for n in range(2, number + 1): # 2 ~ n 까지의 숫자들이 n에 들어가는 것을 반복한다
+#         # 이것들이 소수인가?
+#         for i in prime_list: # 2부터 n-1 까지 i 에 들어가는 것을 반복 (1과 자기 자신 제외)
+#             if n % i == 0: # 나누어 떨어지면 소수가 아니다
+#                 break
+#         else:
+#             prime_list.append(n)
+#     return prime_list
+# 여기서 더 개선할 수 있다.
+# N의 제곱근 보다 크지 않은 어떤 소수로도 나누어 떨어지지 않는다 라는 소수의 개념을 사용하면 된다.
+# i * i <= n 까지 검사하면 된다
+
 def find_prime_list_under_number(number):
+    prime_list = []
 
-    return []
-
+    # 2~number 까지 찾아서 이것들이 소수이면 prime_list 넣는다.
+    for n in range(2, number + 1): # 2 ~ n 까지의 숫자들이 n에 들어가는 것을 반복한다
+        # 이것들이 소수인가?
+        for i in prime_list: # 2부터 n-1 까지 i 에 들어가는 것을 반복 (1과 자기 자신 제외)
+            if i * i <= n and n % i == 0: # i * i <= n 이라는 소수의 개념 + 나누어 떨어지면 소수가 아니다
+                break
+        else:
+            prime_list.append(n)
+    return prime_list
 
 result = find_prime_list_under_number(input)
 print(result)
+
+# 파이썬의 문법 for-else
+# for x in [1,2,3,4]:
+#     print(x)
+#     if x == 4:
+#         break
+# else: # for 문이 정상적으로 모두 완료 되었다면 실행
+#     print("완료되었습니다.")
--- 1st_week/01_homework_02_find_count_to_turn_out_to_all_zero_or_all_one.py
+++ 1st_week/01_homework_02_find_count_to_turn_out_to_all_zero_or_all_one.py
@@ -1,5 +1,7 @@
 # Q.
-# 0과 1로만 이루어진 문자열이 주어졌을 때, 이 문자열에 있는 모든 숫자를 전부 같게 만들려고 한다. 할 수 있는 행동은 문자열에서 연속된 하나 이상의 숫자를 잡고 모두 뒤집는 것이다. 뒤집는 것은 1을 0으로, 0을 1로 바꾸는 것을 의미한다.
+# 0과 1로만 이루어진 문자열이 주어졌을 때, 이 문자열에 있는 모든 숫자를 전부 같게 만들려고 한다.
+# 할 수 있는 행동은 문자열에서 연속된 하나 이상의 숫자를 잡고 모두 뒤집는 것이다.
+# 뒤집는 것은 1을 0으로, 0을 1로 바꾸는 것을 의미한다.
 #
 # 예를 들어 S=0001100 일 때,
 #
@@ -10,12 +12,32 @@
 # 주어진 문자열을 모두 0 혹은 모두 1로 같게 만드는 최소 횟수를 반환하시오.
 
 
-# input = "011110"
+input = "011110"
+
+# 아이디어
+# 언제 0에서 1로 변하는지 1에서 0으로 변하는지가 뒤집는 타이밍이다.
+# 0에서 1로 변할 때 뒤집을지(전체를 0으로), 1에서 0으로 변할 때 뒤집을지(전체를 1로)
 
 def find_count_to_turn_out_to_all_zero_or_all_one(string):
-    # 이 부분을 채워보세요!
-    return 1
+    count_to_all_zero = 0
+    count_to_all_one = 0
 
+    # 맨 첫번째 숫자부터 뒤집는다고 하면
+    if string[0] == '0':
+        count_to_all_one += 1
+    elif string[0] == '1':
+        count_to_all_zero += 1
+
+    for i in range(len(string) - 1): # i 는 0부터 문자열의 길이 - 2 -> 앞에 거와 뒤에 거를 비교해야하므로 맨 마지막 문자 이 전까지만 반복
+        if string[i] != string[i + 1]: # 앞의 숫자와 뒤의 숫자가 다르면 뒤집는 타이밍
+            if string[i + 1] == "1": # 앞은 0이고 뒤는 1이라면
+                count_to_all_zero += 1 # 뒤의 숫자를 1로
+            if string[i + 1] == "0":
+                count_to_all_one += 1
+    print(count_to_all_zero)
+    print(count_to_all_one)
+
+    return min(count_to_all_zero, count_to_all_one)
 
 result = find_count_to_turn_out_to_all_zero_or_all_one(input)
 print(result)
