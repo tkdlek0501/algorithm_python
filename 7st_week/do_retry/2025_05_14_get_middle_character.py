ef6f90cd3d35ad08bd1780b400bc00964aed830b
tkdlek0501 <tkdlek0501@naver.com>
2025-05-14 19:33:18 +0900

    7주차 프로그래머스 level 1 문제 풀이

---
+++ 7st_week/do_retry/2025_05_14_get_middle_character.py
@@ -0,0 +1,25 @@
+# Q. 가운데 글자 가져오기
+# 문제 설명
+# 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+#
+# 재한사항
+# s는 길이가 1 이상, 100이하인 스트링입니다.
+# 입출력 예
+# s	return
+# "abcde"	"c"
+# "qwer"	"we"
+
+def solution(s):
+    center_index = len(s) // 2
+
+    if len(s) % 2 == 0:
+        return s[center_index - 1:center_index + 1]
+    else:
+        return s[center_index]
+
+# <피드백>
+def string_middle(str):
+    if len(str) % 2:
+        return str[len(str) // 2]
+    else:
+        return str[(len(str) // 2) - 1: len(str) // 2 + 1]
